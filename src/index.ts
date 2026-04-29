a38a48cec284e949ec2cdedcccd202df77bf540c
BiggerThings <nnsbiggerthings@gmail.com>
2026-04-29 20:10:22 +0200

    Imported health route into the server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import health_route from "./routes/healthRoute"
 
 const app = express();
 app.use(express.json());
+
+app.use("/health", health_route);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
--- src/routes/healthRoute.ts
+++ src/routes/healthRoute.ts
@@ -4,6 +4,6 @@ import { healthCheckController } from "../controllers/healthCheckController";
 
 const health_route = express.Router();
 
-health_route.get("/health", healthCheckController);
+health_route.get("/", healthCheckController);
 
 export default health_route;
