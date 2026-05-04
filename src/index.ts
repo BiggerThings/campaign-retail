2d37905a588b0cc3eb3ba8c7ce6939c98d55646c
BiggerThings <nnsbiggerthings@gmail.com>
2026-05-04 16:43:10 +0200

    Updated index.ts and docker-compose.yml to inject evn vars into app container

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config()
+
 import express from 'express';
 import health_route from "./routes/healthRoute"
 
@@ -7,6 +10,6 @@ app.use(express.json());
 app.use("/health", health_route);
 
 const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(Number(PORT), '0.0.0.0', () => {
+  console.log(`🚀 Server is running on port ${PORT}`);
 });
