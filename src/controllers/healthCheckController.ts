a3aaffc24a0f05e1edc8299e45b762fe302a630d
BiggerThings <nnsbiggerthings@gmail.com>
2026-05-08 11:49:08 +0200

    Added debug logs in the health check controller

--- src/controllers/healthCheckController.ts
+++ src/controllers/healthCheckController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 
 export const healthCheckController = (req: Request, res: Response) => {
     try {
+        console.log("Health check received ✅✅✅")
         res.status(200).json({
             time: new Date().toISOString(),
             message: "Health check received 🫀🫀🫀"
