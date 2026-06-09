10ff23dc3eb79809f5cfb05226d1864d9f0bcaba
BiggerThings <nnsbiggerthings@gmail.com>
2026-06-09 15:40:27 +0200

    Deleted auth middleware file

--- src/middleware/auth.ts
+++
@@ -1,6 +0,0 @@
-import jwt from 'jsonwebtoken';
-import { Request, Response, NextFunction } from 'express';
-
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-
-}
